7fa74826f505006de1cae2b59bcb3cccf4f06c5e
AmySesh <amysessions16@gmail.com>
2025-01-27 09:13:32 -0700

    this is perfectly valid code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("This is a line of code");
+﻿// commit first then a push pushes it to the cloud 
+Console.WriteLine("This is a line of code");
+
+Console.WriteLine("this is eventually going to be a mistake");
